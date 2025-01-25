60ec7bd8068491645931e40fbdd7d6d5397cc843
rst <roopetimonen@Roopes-MacBook-Pro.local>
2025-01-25 16:49:49 +0200

    Add files previously untracked

---
+++ Assets/_Metsäalkemisti/Scripts/ScriptableObjects/Order.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Order", menuName = "Scriptable Objects/Order")]
+public class Order : ScriptableObject
+{
+    public float wantedTemperature;
+    public float wantedPressure;
+    public float wantedStir;
+
+    public SpriteRenderer hoboSprite;
+}
